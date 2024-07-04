2935af6a0cfa72cc0083b2d3f02621afecd82fa9
Guts <99539689+GutsOfficial@users.noreply.github.com>
2024-07-04 09:01:19 +0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6014602302848340"
+     crossorigin="anonymous"></script>
+</head>
+<body>
+	 
+<br>
+	<div>aaa<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6014602302848340"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-6014602302848340"
+     data-ad-slot="5886721966"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>aaaaaaaaa
+
+
+
+</div>
+
+</body>
+</html>
